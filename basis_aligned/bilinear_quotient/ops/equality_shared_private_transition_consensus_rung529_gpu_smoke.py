288890690a395842168008dc602734e92d870e5f
loganriggs <logan.smith.5@gmail.com>
2026-09-03 11:35:03 +0000

    Expose rung529 predictions to managed gate

--- basis_aligned/bilinear_quotient/ops/equality_shared_private_transition_consensus_rung529_gpu_smoke.py
+++ basis_aligned/bilinear_quotient/ops/equality_shared_private_transition_consensus_rung529_gpu_smoke.py
@@ -9,6 +9,15 @@ import os
 import equality_shared_private_transition_consensus_rung529_run as rung
 
 
+REGISTERED_PREDICTIONS = (
+    "pred_a_exact_live_shared_private_instrument",
+    "pred_b_consensus_beats_every_singleton",
+    "pred_c_new_document_physical_consensus",
+    "pred_d_heldout_circuits_and_documents",
+    "pred_e_sufficient_selectively_removable_shared_state",
+)
+
+
 if os.environ.get("BQLIB_DRYRUN") == "1":
     print(json.dumps(rung.dry_run(), indent=2, sort_keys=True))
 else:
