206ed95d947e6e17052a65835ee7c8a15fd268d6
loganriggs <logan.smith.5@gmail.com>
2026-09-07 10:08:57 +0000

    expose rank46 gate predictions to queue

--- basis_aligned/bilinear_quotient/ops/run_temporal_five_mlp_rank47_pooled_greedy_rank46_deletion_v1.py
+++ basis_aligned/bilinear_quotient/ops/run_temporal_five_mlp_rank47_pooled_greedy_rank46_deletion_v1.py
@@ -8,6 +8,13 @@ import run_temporal_five_mlp_rank48_pooled_joint_rank47_deletion_v1 as engine
 
 ROOT = Path(__file__).resolve().parents[1]
 RANK47 = ROOT / "circuits/followups/temporal_five_mlp_rank48_pooled_joint_rank47_deletion_v1_result.json"
+REGISTERED_PREDICTIONS = (
+    "pred_a_authority_hash_disjointness_finiteness_and_exact_price",
+    "pred_b_at_least_one_rank47_arm_has_bidirectional_coordinate_fidelity",
+    "pred_c_at_least_one_rank47_arm_has_bidirectional_behavior_fidelity",
+    "pred_d_at_least_one_rank47_arm_is_bidirectionally_selective",
+    "pred_e_selected_rank47_arm_is_direction_stable",
+)
 
 engine.PRIOR = ROOT / "circuits/prior_art/temporal_five_mlp_rank47_pooled_greedy_rank46_deletion_v1.json"
 engine.RANK48 = RANK47
